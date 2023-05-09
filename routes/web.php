8b915ab0df36c9e50dded7b661ff1ca7ab3c787a
bunnanvannuth.lim <bunnanvannuth.lim@Student.cadt.edu.kh>
2023-05-09 13:29:54 +0700

    admin template

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Admin\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +21,10 @@ use App\Http\Controllers\HomeController;
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::controller(DashboardController::class)->group(function (){
+    Route::get('/admin/home', 'Index')->name('Home');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
