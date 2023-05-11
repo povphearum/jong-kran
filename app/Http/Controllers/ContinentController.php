2e48169d9d9f249593bd8f7bacbc4f325e5b56be
bunnanvannuth.lim <bunnanvannuth.lim@Student.cadt.edu.kh>
2023-05-12 03:59:31 +0700

    migration

---
+++ app/Http/Controllers/ContinentController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Continent;
+use Illuminate\Http\Request;
+
+class ContinentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Continent $continent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Continent $continent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Continent $continent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Continent $continent)
+    {
+        //
+    }
+}
