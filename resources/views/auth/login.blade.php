afe9ee5f76f20b82e37b9eea7c7af56a43c2d101
MacBook Pro <macbookpro@MacBooks-MacBook-Pro-2.local>
2023-05-25 14:34:14 +0700

    modify sometemplate and logo modify

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,7 +39,7 @@
                     </a>
                 @endif
 
-                <x-button class="ml-4">
+                <x-button class="ml-4 hover:bg-green-600">
                     {{ __('Log in') }}
                 </x-button>
             </div>
