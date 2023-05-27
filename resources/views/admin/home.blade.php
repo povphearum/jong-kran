2f1bfd71036f2fd76d76e4eee4df545ae8730b00
bunnanvannuth.lim <bunnanvannuth.lim@Student.cadt.edu.kh>
2023-05-27 14:40:45 +0700

    modify admin page

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,5 +1,10 @@
-
-<!DOCTYPE html>
+<?php
+function urLIs($value): bool
+{
+    return $_SERVER['REQUEST_URI'] === $value;
+}
+?>
+    <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
@@ -7,7 +12,8 @@
     <title>AdminLTE 3 | Dashboard 2</title>
 
     <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome Icons -->
     <link rel="stylesheet" href="{{asset('Admin/plugins/fontawesome-free/css/all.min.css')}}">
     <!-- overlayScrollbars -->
@@ -24,7 +30,8 @@
 
     <!-- Preloader -->
     <div class="preloader flex-column justify-content-center align-items-center">
-        <img class="animation__wobble" src="{{asset('Admin/dist/img/AdminLTELogo.png')}}" alt="AdminLTELogo" height="60" width="60">
+        <img class="animation__wobble" src="{{asset('Admin/dist/img/AdminLTELogo.png')}}" alt="AdminLTELogo" height="60"
+             width="60">
     </div>
 
     <!-- Navbar -->
@@ -52,7 +59,8 @@
                 <div class="navbar-search-block">
                     <form class="form-inline">
                         <div class="input-group input-group-sm">
-                            <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+                            <input class="form-control form-control-navbar" type="search" placeholder="Search"
+                                   aria-label="Search">
                             <div class="input-group-append">
                                 <button class="btn btn-navbar" type="submit">
                                     <i class="fas fa-search"></i>
@@ -76,7 +84,8 @@
                     <a href="#" class="dropdown-item">
                         <!-- Message Start -->
                         <div class="media">
-                            <img src="{{asset('Admin/')}}dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
+                            <img src="{{asset('Admin/')}}dist/img/user1-128x128.jpg" alt="User Avatar"
+                                 class="img-size-50 mr-3 img-circle">
                             <div class="media-body">
                                 <h3 class="dropdown-item-title">
                                     Brad Diesel
@@ -92,7 +101,8 @@
                     <a href="#" class="dropdown-item">
                         <!-- Message Start -->
                         <div class="media">
-                            <img src="{{asset('Admin/')}}dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+                            <img src="{{asset('Admin/')}}dist/img/user8-128x128.jpg" alt="User Avatar"
+                                 class="img-size-50 img-circle mr-3">
                             <div class="media-body">
                                 <h3 class="dropdown-item-title">
                                     John Pierce
@@ -108,7 +118,8 @@
                     <a href="#" class="dropdown-item">
                         <!-- Message Start -->
                         <div class="media">
-                            <img src="{{asset('Admin/')}}dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+                            <img src="{{asset('Admin/')}}dist/img/user3-128x128.jpg" alt="User Avatar"
+                                 class="img-size-50 img-circle mr-3">
                             <div class="media-body">
                                 <h3 class="dropdown-item-title">
                                     Nora Silvester
@@ -169,7 +180,8 @@
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
         <a href="{{asset('Admin/')}}index3.html" class="brand-link">
-            <img src="{{asset('Admin/')}}dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+            <img src="{{asset('Admin/')}}dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
+                 class="brand-image img-circle elevation-3" style="opacity: .8">
             <span class="brand-text font-weight-light">AdminLTE 3</span>
         </a>
 
@@ -178,7 +190,8 @@
             <!-- Sidebar user panel (optional) -->
             <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                 <div class="image">
-                    <img src="{{asset('Admin/')}}dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                    <img src="{{asset('Admin/')}}dist/img/user2-160x160.jpg" class="img-circle elevation-2"
+                         alt="User Image">
                 </div>
                 <div class="info">
                     <a href="#" class="d-block">Alexander Pierce</a>
@@ -188,7 +201,8 @@
             <!-- SidebarSearch Form -->
             <div class="form-inline">
                 <div class="input-group" data-widget="sidebar-search">
-                    <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+                    <input class="form-control form-control-sidebar" type="search" placeholder="Search"
+                           aria-label="Search">
                     <div class="input-group-append">
                         <button class="btn btn-sidebar">
                             <i class="fas fa-search fa-fw"></i>
@@ -199,12 +213,13 @@
 
             <!-- Sidebar Menu -->
             <nav class="mt-2">
-                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                    data-accordion="false">
                     <!-- Add icons to the links using the .nav-icon class
                          with font-awesome or any other icon font library -->
 
                     <li class="nav-item">
-                        <a href="{{route('adminhome')}}" class="nav-link">
+                        <a href="{{route('adminhome')}}" class="nav-link <?php echo urLIs('/admin/dashboard') ? 'active' : 'nav-link';?>">
                             <i class="nav-icon fas fa-th"></i>
                             <p>
                                 Dashboard
@@ -212,7 +227,7 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a href="{{route('category')}}" class="nav-link">
+                        <a href="{{route('category')}}" class="nav-link <?php echo urLIs('/admin/category') ? 'active' : 'nav-link';?>">
                             <i class="nav-icon fas fa-th"></i>
                             <p>
                                 Category
@@ -220,7 +235,7 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a href="{{route('subcategory')}}" class="nav-link">
+                        <a href="{{route('subcategory')}}" class="nav-link <?php echo urLIs('/admin/subcategory') ? 'active' : 'nav-link';?>">
                             <i class="nav-icon fas fa-th"></i>
                             <p>
                                 Subcategory
@@ -228,7 +243,7 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a href="{{route('recipe')}}" class="nav-link">
+                        <a href="{{route('recipe')}}" class="nav-link <?php echo urLIs('/admin/recipe') ? 'active' : 'nav-link';?>">
                             <i class="nav-icon fas fa-th"></i>
                             <p>
                                 Recipe
@@ -236,15 +251,15 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a href="{{route('user')}}" class="nav-link">
+                        <a href="{{route('user')}}" class="nav-link <?php echo urLIs('/admin/user') ? 'active' : 'nav-link';?>">
                             <i class="nav-icon fas fa-th"></i>
                             <p>
                                 User
                             </p>
                         </a>
                     </li>
-                    <li class="nav-item menu-open">
-                        <a href="#" class="nav-link active">
+                    <li class="nav-item  <?php echo urLIs('/admin/comment') ? 'menu-open' : 'nav-link';?><?php echo urLIs('/admin/reviews') ? 'menu-open' : 'nav-link';?>">
+                        <a href="#" class="nav-link">
                             <i class="nav-icon fas fa-tachometer-alt"></i>
                             <p>
                                 Reviews
@@ -253,13 +268,13 @@
                         </a>
                         <ul class="nav nav-treeview">
                             <li class="nav-item">
-                                <a href="{{route('comment')}}" class="nav-link">
+                                <a href="{{route('comment')}}" class="nav-link <?php echo urLIs('/admin/comment') ? 'active' : 'nav-link';?>">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>comment</p>
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a href="{{route('review')}}" class="nav-link active">
+                                <a href="{{route('review')}}" class="nav-link <?php echo urLIs('/admin/reviews') ? 'active' : 'nav-link';?>">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>Rating</p>
                                 </a>
@@ -288,9 +303,10 @@
 
     <!-- Main Footer -->
     <footer class="main-footer">
-        <strong> Develop By Students of<a href="https://www.cadt.edu.kh/"> Cambodia Academy of Digital Technology</a> . IDT</strong>
+        <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+        All rights reserved.
         <div class="float-right d-none d-sm-inline-block">
-            <strong>Generation_8_Group_B</strong>
+            <b>Version</b> 3.2.0
         </div>
     </footer>
 </div>
